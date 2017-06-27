b9f34b2d18007f83af3c1a64aca74768f70619f9
mohaor <664858757@qq.com>
2017-06-27 11:31:26 +0800

    添加了后台服务器与Matlab服务器交互模块

---
+++ src/com/indoorloc/ShopSignMatching/ClientListenThread.java
@@ -0,0 +1,40 @@
+package com.indoorloc.ShopSignMatching;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ * MatlabClient¼àÌýÏß³Ì
+ * @author Mo Haoran
+ */
+public class ClientListenThread extends Thread {
+	Socket hostSocket;
+	
+	public ClientListenThread(Socket _hostSocket) {
+		hostSocket = _hostSocket;
+	}
+	
+	public void run() {
+		String serverMsg = "";
+		try {
+			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(hostSocket.getInputStream()));
+			serverMsg = inFromServer.readLine();
+			
+			System.out.println("Receive msg from server: " + serverMsg);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Socket Closed.");
+		}
+		
+		try {
+			MatlabClient.setClassTag(serverMsg);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+//		System.out.println("ClientListenThread ended.");
+	}
+}
+
--- src/com/indoorloc/controller/RequestImage.java
+++ src/com/indoorloc/controller/RequestImage.java
@@ -7,16 +7,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.indoorloc.ShopSignMatching.MatlabClient;
 import com.indoorloc.model.ImageDivider;
 import com.indoorloc.model.MyImageIO;
 import com.indoorloc.model.Point;
 
 public class RequestImage extends HttpServlet {
+	MatlabClient matlabClient = new MatlabClient("172.19.128.222", 6677);
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 		
+		//把3张图片的路径传给Matlab服务器，获取类别
+		String[] imagePath = {"5", "2", "4"};
+		try {
+			matlabClient.shopSignClassification(imagePath);
+			Point[] shopPos = matlabClient.getShopPosition();
+			
+			for (int i = 0; i < shopPos.length; i++) {
+				System.out.println("ShopPos" + i + " : (" + shopPos[i].x + ", " + shopPos[i].y + ")");
+			}
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		//回传图片
 		MyImageIO myImageIO = new MyImageIO();
 		
 		//项目部署后的根目录，如 D:\JavaServerProject\.metadata\.me_tcat\webapps\IndoorLocServer\
