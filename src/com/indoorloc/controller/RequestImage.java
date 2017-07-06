23292fb5461c5e9069c1adbc609165e7aec21cfc
mohaor <664858757@qq.com>
2017-07-06 15:51:04 +0800

    注释掉一些东西

--- src/com/indoorloc/controller/RequestImage.java
+++ src/com/indoorloc/controller/RequestImage.java
@@ -13,25 +13,25 @@ import com.indoorloc.model.MyImageIO;
 import com.indoorloc.model.Point;
 
 public class RequestImage extends HttpServlet {
-	MatlabClient matlabClient = new MatlabClient("localhost", 6677);
+//	MatlabClient matlabClient = new MatlabClient("localhost", 6677);
 	
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 		
 		//把3张图片的路径传给Matlab服务器，获取类别
-		String[] imagePath = {"5", "2", "4"};
-		try {
-			matlabClient.shopSignClassification(imagePath);
-			Point[] shopPos = matlabClient.getShopPosition();
-			
-			for (int i = 0; i < shopPos.length; i++) {
-				System.out.println("ShopPos" + i + " : (" + shopPos[i].x + ", " + shopPos[i].y + ")");
-			}
-		} 
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+//		String[] imagePath = {"5", "2", "4"};
+//		try {
+//			matlabClient.shopSignClassification(imagePath);
+//			Point[] shopPos = matlabClient.getShopPosition();
+//			
+//			for (int i = 0; i < shopPos.length; i++) {
+//				System.out.println("ShopPos" + i + " : (" + shopPos[i].x + ", " + shopPos[i].y + ")");
+//			}
+//		} 
+//		catch (Exception e) {
+//			e.printStackTrace();
+//		}
 
 		//回传图片
 		MyImageIO myImageIO = new MyImageIO();
